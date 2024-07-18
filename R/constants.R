65bb6612ff906aa87f996831e6da1dc429463ce9
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-07-18 11:06:17 -0500

    adding a constants file

---
+++ R/constants.R
@@ -0,0 +1,4 @@
+#' Function that returns the awake sleep levels
+#' @return a character vector containing "awake","wake","restless", and "imputed_awake."
+#' @noRd 
+AWAKE_LEVELS <- function() c("awake","wake","restless","imputed_awake")
