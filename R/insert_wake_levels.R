22d37cc89b2eb72ff889f33cb129cd24f98cef20
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-07-25 13:04:00 -0500

    imputed wakes should only be done on tsp=TRUE

--- R/insert_wake_levels.R
+++ R/insert_wake_levels.R
@@ -4,15 +4,16 @@
 insert_wake_levels <- function(all_sleep_dat)
 {
     setkey(all_sleep_dat, person_id, start_datetime)
+    tsp_dat <- all_sleep_dat[is_typical_sleep == TRUE]
     # Insert wakes
     # Add wake between levels
     # Create variable that holds end_time i - 1
-    all_sleep_dat[, end_time_lead_day := shift(end_time,1),.(person_id,date_new)]
+    tsp_dat[, end_time_lead_day := shift(end_time,1),.(person_id,date_new)]
     # Get the difference between start_time and end_time i - 1
-    all_sleep_dat[, lead_diff := as.numeric(start_datetime - end_time_lead_day) / 60]
+    tsp_dat[, lead_diff := as.numeric(start_datetime - end_time_lead_day) / 60]
     # Differences > 0 indicate a gap in the sleep logs
     # Get all gaps
-    wake_between <- all_sleep_dat[lead_diff > 0]
+    wake_between <- tsp_dat[lead_diff > 0]
     # Relabel the variables to create a new wake sleep logs
     wake_between[, end_time := start_datetime]
     wake_between[, start_datetime := end_time_lead_day]
