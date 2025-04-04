6fd8f2afeb91f6a9e6251ff9320371dbcae07dd2
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-04-04 09:45:31 -0500

    updating docs

--- R/interface_get_algorithm_data.R
+++ R/interface_get_algorithm_data.R
@@ -1,19 +1,15 @@
-#' Compute nap metrics for each person_id and date.
+#' Compute algorithm metrics
 #' @param sleep_data an object returned from as_sleep_logs or typical_sleep
-#' @return A dataframe with the following columns (by person_id and date):
+#' @return A dataframe with the following columns:
 #' \describe{ 
-#'  \item{nap_count}{Number of sleep logs outside of primary sleep period}
-#'  \item{nap_length}{Total duration of sleep across sleep logs outside of the primary sleep period}
+#'  \item{person_id}{person ID number}
+#'  \item{median_msp}{Median of the median sleep period}
+#'  \item{median_sleep_start}{Median sleep start}
+#'  \item{median_sleep_end}{Median sleep end}
+#'  \item{bt_wt_by_msp}{Indicates whether the bedtime and waketime were determined by MSP +/- 3.5 hours. This happens when the median BT > median WT}
+#'  \item{bedtime_gt_waketime}{Indicates whether the median BT > median WT} 
 #' }
 #' @export
-#' @examples
-#'\dontrun{
-#' # If parsing from JSON format
-#' dat <- parse_fitbit_json("sleep_data.json")
-#' dat_logs <- as_sleep_logs(dat)
-#' tsp <- get_typical_sleep(dat_logs)
-#' get_algorithm_data(tsp)
-#'}
 #' 
 get_algorithm_data <- function(sleep_data) UseMethod("get_algorithm_data",sleep_data)
 
